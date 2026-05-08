80d4f1d205ad15fdb02c0a03b8bbbb756a2c1a79
Nipun Kaveesha <nipunkaveesha73@gmail.com>
2026-05-08 14:59:27 +0530

    Initial commit

---
+++ run.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int secretNumber = rand() % 100 + 1;
+    int guess = 0;
+    int attempts = 0;
+
+    cout << "Welcome to the Number Guessing Game!" << endl;
+    cout << "I'm thinking of a number between 1 and 100." << endl;
+
+    while (guess != secretNumber) {
+        cout << "\nEnter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess < secretNumber) {
+            cout << "Too low! Try again." << endl;
+        } else if (guess > secretNumber) {
+            cout << "Too high! Try again." << endl;
+        } else {
+            cout << "Correct! You guessed the number in " << attempts << " attempts!" << endl;
+        }
+    }
+
+    return 0;
+}
