b8ca2ce98af2fb5530253b6f0f1046fcacf35f4f
Nipun Kaveesha <nipunkaveesha73@gmail.com>
2026-05-08 15:28:24 +0530

    Allow the user to select a difficulty level

--- run.cpp
+++ run.cpp
@@ -5,13 +5,38 @@
 using namespace std;
 
 int main() {
+
     srand(time(0));
-    int secretNumber = rand() % 100 + 1;
     int guess = 0;
     int attempts = 0;
 
     cout << "Welcome to the Number Guessing Game!" << endl;
-    cout << "I'm thinking of a number between 1 and 100." << endl;
+    cout << "Select a hardness level:" << endl;
+    cout << "1. Easy (1-100)" << endl;
+    cout << "2. Medium (1-500)" << endl;
+    cout << "3. Hard (1-1000)" << endl;
+
+    int level;
+    cin >> level;
+
+    int maxNumber;
+    switch (level) {
+        case 1:
+            maxNumber = 100;
+            break;
+        case 2:
+            maxNumber = 500;
+            break;
+        case 3:
+            maxNumber = 1000;
+            break;
+        default:
+            maxNumber = 100;
+    }
+
+    int secretNumber = rand() % maxNumber + 1;
+
+    cout << "I'm thinking of a number between 1 and " << maxNumber << "." << endl;
 
     while (guess != secretNumber) {
         cout << "\nEnter your guess: ";
